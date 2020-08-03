e8603e54cccc3998bada0f8a43aeb4c22b5f9e6d
Abdulaziz Ladan <abdulazizladan@gmail.com>
2020-08-03 14:15:17 +0100

    feat: Complete view Inbox and outbox

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -21,11 +21,21 @@ class MessageController extends Controller
     }
 
     function messageInbox(Request $request){
-        return array("message" => "Inbox!", "status" => 200);
+        $data = $request->validate([
+           "recipient_id" => ['required']
+        ]);
+        return Message::where('recipient_id', $request->recipient_id)
+            ->join('users', 'users.id', 'messages.recipient_id')
+            ->get();
     }
 
     function messageOutbox(Request $request){
-        return array("message" => "Showing outbox", "status" => 200);
+        $data = $request->validate([
+            "sender_id" => ['required']
+        ]);
+        return Message::where('sender_id', $request->sender_id)
+            ->join('users', 'users.id', 'messages.sender_id')
+            ->get();
     }
 
     function readSingleMessage(Request $request){
